292b20230b897a2fbe42bfa17d6f8bdd8b8ea434
Zuzana Bojova <zbojova@gmail.com>
2024-02-22 23:11:31 +0000

    update

--- script.js
+++ script.js
@@ -36,8 +36,17 @@ taskForm.addEventListener("submit", (e) => {
   }
 
   taskData.forEach(({id, title, date, description}) => {
-      (tasksContainer.innerHTML += `
-      `)
-    }
-  );
+    (tasksContainer.innerHTML += `
+      <div class="task" id="${id}">
+        <p><strong>Title:</strong> ${title}</p>
+        <p><strong>Date:</strong> ${date}</p>
+        <p><strong>Description:</strong> ${description}</p>
+        <button type="button" class="btn">Edit</button>
+        <button type="button" class="btn">Delete</button>
+      </div>
+    `)
+  } 
+);
+
+taskForm.classList.toggle("hidden");
 });
